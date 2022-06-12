e4853aaeff5ab9b86c92831e9198ff909798c0c1
santitomatis <santiagotomatis06@gmail.com>
2022-06-11 22:49:42 -0300

    functions implemented, now it can also convert from MX to USD and vice versa

--- conversorMonedas.py
+++ conversorMonedas.py
@@ -1,21 +1,46 @@
-valorDolar = 0.004878
-valorPeso = 205
-print("Quieres convertir pesos a dolares o dolares a pesos? (escribe <pesos a dolares> o <dolares a pesos> (sin <>))")
-convertir = input("")
+menu = """
+Welcome! This is your currency converter
+1- convert from USD to ARS
+2- convert from ARS to USD
+3- convert from USD to MX
+4- convert from MX to USD
 
-if convertir == "pesos a dolares":
-    print("¿Cuantos pesos argentinos tienes?")
-    pesos_argentinos = int(input(""))
-    dolares = round(pesos_argentinos * valorDolar)
-    print("Tienes aproximadamente " + str(dolares) + " dolares")
+Type <1> <2> <3> <4> according to the convert action required (without using <>)"""
+print(menu)
+convert = int(input(""))
+value_ARS_USD = 0.004878
+value_USD_ARS = 205
+value_MX_USD = 0.050
+value_USD_MX = 20
 
-if convertir == "dolares a pesos":
-    print("¿Cuantos dolares tienes?")
-    dolares = int(input(""))
-    pesos_argentinos = round(dolares * valorPeso)
-    print("Tienes aproximadamente " + str(pesos_argentinos) + " pesos")
+def converter(currency_to_convert, value_currency, currency_output):
+    print("How many " + currency_to_convert + " do you have?")
+    to_convert = int(input(""))
+    converted = round(to_convert * value_currency)
+    print("You got about " + str(converted) + " " + currency_output)
 
-else:
-    print("comando no valido, vuelve a ejecutar el programa y escribe bien")
+if convert == 1:
+    currency_to_convert = "USD"
+    currency_output = "ARS"
+    converter(currency_to_convert, value_USD_ARS, currency_output)
+
+if convert == 2:
+    currency_to_convert = "ARS"
+    currency_output = "USD"
+    converter(currency_to_convert, value_ARS_USD, currency_output)
+
+if convert == 3:
+    currency_to_convert = "USD"
+    currency_output = "MX"
+    converter(currency_to_convert, value_USD_MX, currency_output)
+
+if convert == 4:
+    currency_to_convert = "MX"
+    currency_output = "USD"
+    converter(currency_to_convert, value_MX_USD, currency_output)
 
 
+
+
+# else:
+#     print("comando no valido, vuelve a ejecutar el programa y escribe bien")
