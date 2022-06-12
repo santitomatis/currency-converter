c5906f178fde99afefe59a1c753979dad9c0a040
santitomatis <santiagotomatis06@gmail.com>
2022-06-11 22:14:20 -0300

    primera version del conversor de monedas

---
+++ conversorMonedas.py
@@ -0,0 +1,7 @@
+print("¿Cuantos pesos argentinos tienes?")
+pesos_argentinos = int(input(""))
+valorDolar = 0.004878
+dolares = round(pesos_argentinos * valorDolar)
+print("Tienes aproximadamente " + str(dolares) + " dolares")
+
+
