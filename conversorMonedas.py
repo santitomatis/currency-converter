d09a9875c8b607524f57ff905c9946f0513b998e
santitomatis <santiagotomatis06@gmail.com>
2022-06-11 22:15:21 -0300

    ahora se puede elegir si pasar de pesos a dolares o dolares a pesos

--- conversorMonedas.py
+++ conversorMonedas.py
@@ -1,7 +1,21 @@
-print("¿Cuantos pesos argentinos tienes?")
-pesos_argentinos = int(input(""))
 valorDolar = 0.004878
-dolares = round(pesos_argentinos * valorDolar)
-print("Tienes aproximadamente " + str(dolares) + " dolares")
+valorPeso = 205
+print("Quieres convertir pesos a dolares o dolares a pesos? (escribe <pesos a dolares> o <dolares a pesos> (sin <>))")
+convertir = input("")
+
+if convertir == "pesos a dolares":
+    print("¿Cuantos pesos argentinos tienes?")
+    pesos_argentinos = int(input(""))
+    dolares = round(pesos_argentinos * valorDolar)
+    print("Tienes aproximadamente " + str(dolares) + " dolares")
+
+if convertir == "dolares a pesos":
+    print("¿Cuantos dolares tienes?")
+    dolares = int(input(""))
+    pesos_argentinos = round(dolares * valorPeso)
+    print("Tienes aproximadamente " + str(pesos_argentinos) + " pesos")
+
+else:
+    print("comando no valido, vuelve a ejecutar el programa y escribe bien")
 
 
